61f47a3f181e08a028e30803b6342397e3113b26
Matheus Amaro <matheusamaros@hotmail.com>
2021-08-06 17:46:33 -0300

    alterado texto

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Fala Galera!");
+            Console.WriteLine("Oi mundo");
 
         }
     }
